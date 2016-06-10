3acf3f7b93af467209872c2f4fc95ddd1ed0dd3b
Xavid Ramirez <xavid.ramirez01@utrgv.edu>
2016-06-10 11:13:57 -0500

    fixed mac finding script

--- PythonScripts/findMac.py
+++ PythonScripts/findMac.py
@@ -1,7 +1,7 @@
 import os, commands
 
 def myMAC(iface):
-	words = commands.getoutput("ifconfig" + iface).split()
+	words = commands.getoutput("ifconfig " + iface).split()
 	if "HWaddr" in words:
 		return words[words.index("HWaddr") + 1]
 	else:
